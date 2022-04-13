89872eec104edfc8669f2318bc5d8dd51b288a43
charlesdoiron <doironcharles@gmail.com>
2022-04-13 10:22:06 +0200

    add graphql config

--- config/plugins.js
+++ config/plugins.js
@@ -4,6 +4,8 @@ module.exports = {
   // GQL
   graphql: {
     config: {
+      endpoint: "/graphql",
+      shadowCRUD: true,
       playgroundAlways: true,
       defaultLimit: 500,
       maxLimit: 10000,
