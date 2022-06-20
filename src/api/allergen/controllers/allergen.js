8ee838eed7faa2ff51db8691e437d09974b98183
charlesdoiron <doironcharles@gmail.com>
2022-06-20 16:56:15 +0200

    clean

--- src/api/allergen/controllers/allergen.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  allergen controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::allergen.allergen');
