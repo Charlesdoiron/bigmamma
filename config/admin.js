aaf9a5d4ba9fdda1af3c2b4fa82274e1bac7235b
charlesdoiron <doironcharles@gmail.com>
2022-06-20 09:10:40 +0200

    fix env

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,9 @@
 module.exports = ({ env }) => ({
   auth: {
-    // secret: env('ADMIN_JWT_SECRET', 'd177cf700ba36402131bf543ddafc1bd'),
     secret: env(
       "ADMIN_JWT_SECRET" || crypto.randomBytes(16).toString("base64")
     ),
   },
 });
+
+// secret: env('ADMIN_JWT_SECRET', 'd177cf700ba36402131bf543ddafc1bd'),
