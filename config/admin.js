68f051fffda0713cdfa69b907b0ed7f5b2901c2c
charlesdoiron <doironcharles@gmail.com>
2022-06-20 09:30:45 +0200

    try jwt

--- config/admin.js
+++ config/admin.js
@@ -1,4 +1,9 @@
+const crypto = require("crypto");
+
 module.exports = ({ env }) => ({
+  apiToken: {
+    salt: env("API_TOKEN_SALT", "someRandomLongString"),
+  },
   auth: {
     secret: env(
       "ADMIN_JWT_SECRET" || crypto.randomBytes(16).toString("base64")
